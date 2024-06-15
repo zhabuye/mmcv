14204ff1cd98ff4eb0cf931a305931ce3e0d52fd
zhabuye <74179177+zhabuye@users.noreply.github.com>
2024-06-15 17:48:52 +0800

    Update three_nn.py

--- mmcv/ops/three_nn.py
+++ mmcv/ops/three_nn.py
@@ -47,7 +47,7 @@ class ThreeNN(Function):
             dist2 = torch.sqrt(dist2)
             if dtype_ == torch.float16:
                 dist2 = dist2.half()
-            return dist2, idx.type(torch.IntTensor)
+            return dist2, idx.type(torch.IntTensor).npu()
         dist2 = target.new_empty(B, N, 3)
         idx = target.new_empty(B, N, 3, dtype=torch.int32)
 
